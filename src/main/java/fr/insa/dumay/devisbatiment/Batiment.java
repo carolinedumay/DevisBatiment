575cb6d28543b233edb9be8bc720da238577920a
mfanget01 <mfanget01@pc-0281.csip.in>
2023-04-06 12:22:32 +0200

    partage du projet

---
+++ src/main/java/fr/insa/dumay/devisbatiment/Batiment.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.insa.dumay.devisbatiment;
+
+/**
+ *
+ * @author mfanget01
+ */
+public class Batiment {
+    
+/// Attributs ///
+    String idBatiment;
+    //Niveau listeNiveaux[];
+    
+/// Constructeurs ///
+    Batiment(String ID)
+    {
+        this.idBatiment = ID;
+    }
+    
+/// Methodes ///
+    void afficher()
+    {
+        
+    }
+    
+    void sauvegarder()
+    {
+    
+    }
+    
+    float devisBatiment() //float ?
+    {
+        return 0; //temporaire
+    }        
+            
+    void Dessiner()
+    {
+        
+    }
+}
