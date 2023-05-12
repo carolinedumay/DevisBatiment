40a78748f64ae2fe3b8f6c0a2824a8ed017ccd2f
juliette <juliette@port-info-2.ch-cahors.local>
2023-05-12 10:08:36 +0200

    début1 interface graphique

--- src/main/java/fr/insa/dumay/devisbatiment/interfacegraphique/Main.java
+++ src/main/java/fr/insa/dumay/devisbatiment/interfacegraphique/Main.java
@@ -10,7 +10,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
+import java.io.*;
 /**
  *
  * @author juliette
@@ -31,6 +31,49 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch();
+        
+        //création du fichier texte batiment.txt
+        try
+        {
+        BufferedWriter batiment=new BufferedWriter(new FileWriter("batiment.txt",true));
+        batiment.write("Coin;1;5,26;16,25");
+        batiment.newLine();
+        batiment.write("Mur;1;1;2;");
+        batiment.newLine();
+        batiment.close();
+        }
+        catch(IOException erreur)
+        {
+            System.out.println("erreur fichier");
+        }
+        //Lecture du fichier texte catalogueRevetement.txt
+        //recherche de revetement par désignation
+        String designation="Peinture";
+        String[] t;
+        try
+        {
+            BufferedReader revetement=new BufferedReader(new FileReader("CatalogueRevetement.txt"));
+            String lignelue="abc";
+            while((lignelue=revetement.readLine())!=null)
+                    {
+                        t=lignelue.split(";");
+                        if(t[0].equals(designation))
+                        {
+                        System.out.println(lignelue);
+                        System.out.println("Prix unitaire:"+t[5]);// la position du prix unitaire est à t5
+                        //conversion du prix en type de donnée double
+                        double pu=Double.parseDouble(t[5]);
+                        double surface=15;
+                        double devisRevetement=surface*pu;
+                        System.out.println("devis Revetement:"+devisRevetement);
+                        }
+                    }
+            //revetement.readLine();
+        }
+        
+        catch(IOException erreurLecture)
+        {
+            System.out.println("erreur de lecture du fichier");
+        }
     }
-
 }
