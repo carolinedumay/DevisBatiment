b0920b2a3411e032e4916a70f960a0184b8e5da7
juliette <juliette@port-info-2.ch-cahors.local>
2023-05-23 00:02:58 +0200

    début classe fenetre de l'interface

---
+++ src/main/java/fr/insa/dumay/devisbatiment/interfacegraphique/FOuverture.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.insa.dumay.devisbatiment.interfacegraphique;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
+/**
+ *
+ * @author juliette
+ */
+public class FOuverture extends MainPane {
+    /**public FOuverture() {
+        setTitle("Ouverture");
+
+        Button closeButton = new Button("Fermer");
+        closeButton.setOnAction(event -> close());
+
+        VBox vbox = new VBox();
+        vbox.getChildren().addAll(new Button("Cliquez-moi"), closeButton);
+
+        Scene scene = new Scene(vbox, 300, 200);
+        setScene(scene);
+    }
+
+    
+    
+    
+    
+    
+    private void setTitle(String fenêtre_secondaire) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private void close() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private void setScene(Scene scene) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    */
+}
